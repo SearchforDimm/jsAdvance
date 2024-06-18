8224ffcfe883f8d7f44c1611cd5e822296e8b189
Dimm <dmzrfqi@gmail.com>
2024-06-18 12:33:46 +0700

    creating object with class and prototype inheritence

--- object/2/script.js
+++ object/2/script.js
@@ -21,32 +21,79 @@ let mahasiswa2 = {
 
 // 2. function declaration
 
-const methodMahasiswa = {
-    makan : function(porsi) {
-        this.energi += porsi;
-        console.log(`halo ${this.nama}, selamat makan!`)
-    },
+// const methodMahasiswa = {
+//     makan : function(porsi) {
+//         this.energi += porsi;
+//         console.log(`halo ${this.nama}, selamat makan!`)
+//     },
 
-    main: function(jam) {
-        this.energi -+ jam;
-    },
-    tidur: function(jam) {
-        this.energi += jam * 2;
-    },
-}
+//     main: function(jam) {
+//         this.energi -+ jam;
+//     },
+//     tidur: function(jam) {
+//         this.energi += jam * 2;
+//     },
+// }
 
-function Mahasiwa(nama, energi) {
-    let mahasiswa = Object.create(methodMahasiswa);
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi
+// function Mahasiwa(nama, energi) {
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi
     
 
  
-    return mahasiswa;  // must return if using function declaration
+//     return mahasiswa;  // must return if using function declaration
+// }
+
+
+// versi prototype inheritence
+// function Mahasiswa(nama, energi) {
+//     // let mahasiswa = Object.create(methodMahasiswa);
+//     // let this = Object.create(Mahasiswa.prototype);  //auto created when using "this"
+//     this.nama = nama;
+//     this.energi = energi
+// }
+// Mahasiswa.prototype.makan = function(porsi) {
+//     this.energi += porsi;
+//     return `halo ${this.nama}, selamat makan`
+// }
+// Mahasiswa.prototype.main = function(jam) {
+//     this.energi -= jam;
+//     return `halo ${this.nama}, selamat main`
+// }
+// Mahasiswa.prototype.tidur = function(jam) {
+//     this.energi += jam * 2;
+//     return `halo ${this.nama}, selamat tidur`
+// }
+
+
+// versi Class
+class Mahasiswa {
+    constructor(nama, energi) {
+        this.nama = nama;
+        this.energi = energi;
+    }
+
+
+    makan(porsi) {
+    this.energi += porsi;
+    return `halo ${this.nama}, selamat makan`
+}
+    main(jam) {
+    this.energi -= jam;
+    return `halo ${this.nama}, selamat main`
+}
+    tidur(jam){
+    this.energi += jam * 2;
+    return `halo ${this.nama}, selamat tidur`
+    }
 }
 
-let pierre = Mahasiwa("Pierre", 20)
+let pierre = new Mahasiswa("dimas", 10);
 
+
+// let ang = [];
+let ang = new Array();
 // 3. Constructor Function
 
 // function Mahasiswa(nama, energi) {
